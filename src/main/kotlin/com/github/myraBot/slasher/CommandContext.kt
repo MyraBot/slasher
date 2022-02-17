135d164de22f909f0abe6574beb0cf5b15b9c841
Marian <elias-marian@web.de>
2022-02-17 18:56:57 +0100

    :wheelchair: Switched to GuildSlashCommandEvents & Made CommandContext extend GuildSlashCommandEvent

--- src/main/kotlin/com/github/myraBot/slasher/CommandContext.kt
+++ src/main/kotlin/com/github/myraBot/slasher/CommandContext.kt
@@ -1,29 +1,19 @@
 package com.github.myraBot.slasher
 
-import com.github.myraBot.diskord.common.entities.User
 import com.github.myraBot.diskord.common.entities.applicationCommands.Interaction
-import com.github.myraBot.diskord.common.entities.applicationCommands.slashCommands.SlashCommand
-import com.github.myraBot.diskord.common.entities.channel.TextChannel
 import com.github.myraBot.diskord.common.entities.guild.Guild
 import com.github.myraBot.diskord.common.entities.guild.Member
-import com.github.myraBot.diskord.gateway.listeners.impl.interactions.SlashCommandEvent
+import com.github.myraBot.diskord.gateway.listeners.impl.interactions.slashCommands.GuildSlashCommandEvent
 import com.github.myraBot.diskord.rest.behaviors.InteractionCreateBehavior
 import kotlinx.coroutines.runBlocking
 import kotlin.reflect.KFunction
 
 @Suppress("unused")
 class CommandContext internal constructor(
-        val event: SlashCommandEvent,
-        val method: KFunction<*>,
-        private val _command: CommandImpl,
-) : InteractionCreateBehavior {
-    override val interaction: Interaction = event.interaction
-    val command: SlashCommand = event.command
-
-    val channel: TextChannel get() = runBlocking { event.channel.awaitNonNull() }
-    val user: User = event.member!!.user //TODO get uesr directly
-
-    val guild: Guild get() = runBlocking { event.guild.awaitNonNull() }
-    val member: Member = event.member!!
+    data: Interaction,
+    val method: KFunction<*>,
+    private val _command: CommandImpl,
+) : GuildSlashCommandEvent(data), InteractionCreateBehavior {
+    val guild: Guild get() = runBlocking { super.getGuild().awaitNonNull() }
     val botMember: Member get() = runBlocking { guild.getBotMember().awaitNonNull() }
 }
--- src/main/kotlin/com/github/myraBot/slasher/Handler.kt
+++ src/main/kotlin/com/github/myraBot/slasher/Handler.kt
@@ -2,15 +2,15 @@
 
 package com.github.myraBot.slasher
 
+import bot.myra.kommons.trace
 import com.github.myraBot.diskord.common.entities.Role
 import com.github.myraBot.diskord.common.entities.User
 import com.github.myraBot.diskord.common.entities.applicationCommands.slashCommands.SlashCommandOptionType
 import com.github.myraBot.diskord.common.entities.channel.ChannelData
 import com.github.myraBot.diskord.common.entities.guild.Member
-import com.github.myraBot.diskord.common.utilities.trace
 import com.github.myraBot.diskord.gateway.listeners.EventListener
 import com.github.myraBot.diskord.gateway.listeners.ListenTo
-import com.github.myraBot.diskord.gateway.listeners.impl.interactions.SlashCommandEvent
+import com.github.myraBot.diskord.gateway.listeners.impl.interactions.slashCommands.GuildSlashCommandEvent
 import org.reflections.Reflections
 import kotlin.reflect.KClass
 import kotlin.reflect.KFunction
@@ -20,10 +20,10 @@ class Handler : EventListener {
     private val cogs: MutableList<Cog> = mutableListOf()
     var commandPackage: String? = null
 
-    @ListenTo(SlashCommandEvent::class)
-    suspend fun onMessage(event: SlashCommandEvent) = handle(event)
+    @ListenTo(GuildSlashCommandEvent::class)
+    suspend fun onMessage(event: GuildSlashCommandEvent) = handle(event)
 
-    private suspend fun handle(event: SlashCommandEvent) {
+    private suspend fun handle(event: GuildSlashCommandEvent) {
         val name = StringBuilder(event.command.name).apply {
             event.command.options.forEach {
                 if (it.type == SlashCommandOptionType.SUB_COMMAND || it.type == SlashCommandOptionType.SUB_COMMAND_GROUP) {
@@ -45,11 +45,11 @@ class Handler : EventListener {
                         resolveParam(klass, option.name, event)
                     }
 
-                command.method.callSuspend(cog, CommandContext(event, command.method, command), *args.toTypedArray())
+                command.method.callSuspend(cog, CommandContext(event.interaction, command.method, command), *args.toTypedArray())
             }
     }
 
-    private fun resolveParam(klass: KClass<*>, name: String, event: SlashCommandEvent): Any? = when (klass) {
+    private fun resolveParam(klass: KClass<*>, name: String, event: GuildSlashCommandEvent): Any? = when (klass) {
         String::class -> event.getOption<String>(name)
         Int::class -> event.getOption<Int>(name)
         Boolean::class -> event.getOption<Boolean>(name)
